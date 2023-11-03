f697fd37bf906bdce91b5081ba29c337171083c3
Ashish Upadhaya <54333928+Bless-ashish@users.noreply.github.com>
2023-11-03 19:02:07 +0100

    Delete Commandline_CGPT.sh

--- Commandline_CGPT.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "series.json" ]; then
-  echo "Error: 'series.json' file not found."
-  exit 1
-fi
-
-jq -r '. as $parent | .works[] | [$parent.id, $parent.title, .books_count] | join("|")' series.json | awk -F'|' '{a[$1"|"$2]+=$3} END{for (i in a) print i"| "a[i]}' | sort -t'|' -k3nr | head -n 5
